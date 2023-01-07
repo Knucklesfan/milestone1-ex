7eed5b2b57863aafb10e7dccffb652b988fa63b5
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2023-01-07 15:52:42 -0600

    continued SDL2 implementation, continued work on cmake

---
+++ src/CMakeFiles/supertux.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/badguy.cpp" "CMakeFiles/supertux.dir/badguy.cpp.o" "gcc" "CMakeFiles/supertux.dir/badguy.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/button.cpp" "CMakeFiles/supertux.dir/button.cpp.o" "gcc" "CMakeFiles/supertux.dir/button.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/collision.cpp" "CMakeFiles/supertux.dir/collision.cpp.o" "gcc" "CMakeFiles/supertux.dir/collision.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/configfile.cpp" "CMakeFiles/supertux.dir/configfile.cpp.o" "gcc" "CMakeFiles/supertux.dir/configfile.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/gameloop.cpp" "CMakeFiles/supertux.dir/gameloop.cpp.o" "gcc" "CMakeFiles/supertux.dir/gameloop.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/gameobjs.cpp" "CMakeFiles/supertux.dir/gameobjs.cpp.o" "gcc" "CMakeFiles/supertux.dir/gameobjs.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/globals.cpp" "CMakeFiles/supertux.dir/globals.cpp.o" "gcc" "CMakeFiles/supertux.dir/globals.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/high_scores.cpp" "CMakeFiles/supertux.dir/high_scores.cpp.o" "gcc" "CMakeFiles/supertux.dir/high_scores.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/intro.cpp" "CMakeFiles/supertux.dir/intro.cpp.o" "gcc" "CMakeFiles/supertux.dir/intro.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/level.cpp" "CMakeFiles/supertux.dir/level.cpp.o" "gcc" "CMakeFiles/supertux.dir/level.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/leveleditor.cpp" "CMakeFiles/supertux.dir/leveleditor.cpp.o" "gcc" "CMakeFiles/supertux.dir/leveleditor.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/lispreader.cpp" "CMakeFiles/supertux.dir/lispreader.cpp.o" "gcc" "CMakeFiles/supertux.dir/lispreader.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/menu.cpp" "CMakeFiles/supertux.dir/menu.cpp.o" "gcc" "CMakeFiles/supertux.dir/menu.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/mousecursor.cpp" "CMakeFiles/supertux.dir/mousecursor.cpp.o" "gcc" "CMakeFiles/supertux.dir/mousecursor.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/music_manager.cpp" "CMakeFiles/supertux.dir/music_manager.cpp.o" "gcc" "CMakeFiles/supertux.dir/music_manager.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/musicref.cpp" "CMakeFiles/supertux.dir/musicref.cpp.o" "gcc" "CMakeFiles/supertux.dir/musicref.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/particlesystem.cpp" "CMakeFiles/supertux.dir/particlesystem.cpp.o" "gcc" "CMakeFiles/supertux.dir/particlesystem.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/physic.cpp" "CMakeFiles/supertux.dir/physic.cpp.o" "gcc" "CMakeFiles/supertux.dir/physic.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/player.cpp" "CMakeFiles/supertux.dir/player.cpp.o" "gcc" "CMakeFiles/supertux.dir/player.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/resources.cpp" "CMakeFiles/supertux.dir/resources.cpp.o" "gcc" "CMakeFiles/supertux.dir/resources.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/scene.cpp" "CMakeFiles/supertux.dir/scene.cpp.o" "gcc" "CMakeFiles/supertux.dir/scene.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/screen.cpp" "CMakeFiles/supertux.dir/screen.cpp.o" "gcc" "CMakeFiles/supertux.dir/screen.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/setup.cpp" "CMakeFiles/supertux.dir/setup.cpp.o" "gcc" "CMakeFiles/supertux.dir/setup.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/sound.cpp" "CMakeFiles/supertux.dir/sound.cpp.o" "gcc" "CMakeFiles/supertux.dir/sound.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/special.cpp" "CMakeFiles/supertux.dir/special.cpp.o" "gcc" "CMakeFiles/supertux.dir/special.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/sprite.cpp" "CMakeFiles/supertux.dir/sprite.cpp.o" "gcc" "CMakeFiles/supertux.dir/sprite.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/sprite_manager.cpp" "CMakeFiles/supertux.dir/sprite_manager.cpp.o" "gcc" "CMakeFiles/supertux.dir/sprite_manager.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/supertux.cpp" "CMakeFiles/supertux.dir/supertux.cpp.o" "gcc" "CMakeFiles/supertux.dir/supertux.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/text.cpp" "CMakeFiles/supertux.dir/text.cpp.o" "gcc" "CMakeFiles/supertux.dir/text.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/texture.cpp" "CMakeFiles/supertux.dir/texture.cpp.o" "gcc" "CMakeFiles/supertux.dir/texture.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/tile.cpp" "CMakeFiles/supertux.dir/tile.cpp.o" "gcc" "CMakeFiles/supertux.dir/tile.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/timer.cpp" "CMakeFiles/supertux.dir/timer.cpp.o" "gcc" "CMakeFiles/supertux.dir/timer.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/title.cpp" "CMakeFiles/supertux.dir/title.cpp.o" "gcc" "CMakeFiles/supertux.dir/title.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/type.cpp" "CMakeFiles/supertux.dir/type.cpp.o" "gcc" "CMakeFiles/supertux.dir/type.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/world.cpp" "CMakeFiles/supertux.dir/world.cpp.o" "gcc" "CMakeFiles/supertux.dir/world.cpp.o.d"
+  "/home/knucklesfan/Documents/GitHub/milestone1-ex/src/worldmap.cpp" "CMakeFiles/supertux.dir/worldmap.cpp.o" "gcc" "CMakeFiles/supertux.dir/worldmap.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
